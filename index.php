94d84b798ca355b18e5a06037656562da3b24dd3
alaca <ante.laca@gmail.com>
2016-05-19 08:18:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ include 'localization.php';
 
 // settings
 $localization = Localization::settings([
-   'path'  => 'translations', // translations directory path | translations is default
-   'input' => 'language',     // url parameter | language is default
    'languages' => [           // languages, first language is default
       'en' => 'English',
       'de' => 'Deutsch',
