26139b08a3fc148861f3fd1446f0237007a9a91e
alaca <ante.laca@gmail.com>
2016-05-19 22:27:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,34 +14,34 @@ $localization = Localization::settings([
 
 ?>
 <html lang="<?php echo $localization->getCurrentLanguage(); ?>">
-   <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      <title><?php echo __('Site title'); ?></title>
-   </head>
-   <body>
-      <h1><?php echo __('Welcome to our site'); ?></h1>
-      <hr />
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <title><?php echo __('Site title'); ?></title>
+</head>
+<body>
+   <h1><?php echo __('Welcome to our site'); ?></h1>
+   <hr />
       
-      <ul>
-         <?php foreach($localization->getLanguages() as $code => $name): ?>
-            <li>
-               <a href="?language=<?php echo $code; ?>"><?php echo $name; ?></a>
-            </li>
-         <?php endforeach; ?>
-      </ul>
+   <ul>
+      <?php foreach($localization->getLanguages() as $code => $name): ?>
+         <li>
+            <a href="?language=<?php echo $code; ?>"><?php echo $name; ?></a>
+         </li>
+      <?php endforeach; ?>
+   </ul>
       
-      <hr />
+   <hr />
       
-      <?php echo __('Current language key code is %s', [$localization->getCurrentLanguage()]); ?>
-      <br /><br />
+   <?php echo __('Current language key code is %s', [$localization->getCurrentLanguage()]); ?>
+   <br /><br />
       
-      <?php echo __('Translate function can take multiple params like %s and %s', ['this', 'that']); ?>
-      <br /><br />
+   <?php echo __('Translate function can take multiple params like %s and %s', ['this', 'that']); ?>
+   <br /><br />
       
-      <?php echo Localization::instance()->translate('You can call it like this'); ?> ;
-      <br /><br />
+   <?php echo Localization::instance()->translate('You can call it like this'); ?> ;
+   <br /><br />
       
       
       
-   </body>
+</body>
 </html>
