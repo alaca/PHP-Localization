836ffbf2ea4ea83c8643c71ba24716283d2b5b7b
alaca <ante.laca@gmail.com>
2016-05-20 17:45:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'localization.php';
 
 // settings
 $localization = Localization::settings([
-   'languages' => [           // languages, first language is default
+   'languages' => [
       'en' => 'English',
       'de' => 'Deutsch',
       'it' => 'Italiano'
@@ -17,11 +17,20 @@ $localization = Localization::settings([
 <head>
    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
    <title><?php echo __('Site title'); ?></title>
+   <script>
+   
+      /* method getJson() can be handy to translate javascript */
+      var translations = <?php echo $localization->getJson(['Welcome to our site', 'Site title']); ?>;
+      
+      /* then you can use it like this */
+      console.log(translations['Welcome to our site']);
+      
+   </script>
 </head>
 <body>
    <h1><?php echo __('Welcome to our site'); ?></h1>
    <hr />
-      
+   
    <ul>
       <?php foreach($localization->getLanguages() as $code => $name): ?>
          <li>
@@ -29,19 +38,19 @@ $localization = Localization::settings([
          </li>
       <?php endforeach; ?>
    </ul>
-      
+   
    <hr />
-      
+   
    <?php echo __('Current language key code is %s', [$localization->getCurrentLanguage()]); ?>
    <br /><br />
-      
+   
    <?php echo __('Translate function can take multiple params like %s and %s', ['this', 'that']); ?>
    <br /><br />
-      
+   
    <?php echo Localization::instance()->translate('You can call it like this'); ?> ;
    <br /><br />
-      
-      
-      
+   
+   
+   
 </body>
 </html>
