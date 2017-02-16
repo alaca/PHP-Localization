f3904d3cc3e92440f674b2d80e8b3bd16e353a7c
alaca <ante.laca@gmail.com>
2017-02-16 08:19:00 +0100

    Update localization.php

--- localization.php
+++ localization.php
@@ -11,145 +11,145 @@
 
 class Localization {
 
-	private static 
-		$instance = null,
-		$settings = [],
-		$translations = [];
+    private static 
+	$instance = null,
+	$settings = [],
+	$translations = [];
 
-	private $translationFile;	  	  	  
+    private $translationFile;	  	  	  
 
 
-	private function __construct()
-	{
+    private function __construct()
+    {
 
-		static::$settings = require 'config.php';
+	static::$settings = require 'config.php';
 
-		$this->translationFile = static::$settings['path'] . '/' . $this->getCurrentLanguage() . '.ini';
+	$this->translationFile = static::$settings['path'] . '/' . $this->getCurrentLanguage() . '.ini';
 
-		if (is_dir(dirname($this->translationFile))) {
+	if (is_dir(dirname($this->translationFile))) {
          
-			if (file_exists($this->translationFile)) {
-				static::$translations = parse_ini_file($this->translationFile);
-			} else {
-				if (file_exists($default = static::$settings['path'] . '/' . $this->getDefaultLanguage() . '.ini')) {
-					copy($default, $this->translationFile);
-				}
-			}  
-
-		} else {
-
-			printf('Directory <strong>%s</strong> not exists, check path setting', dirname($this->translationFile)); 
-			exit;
-
-		}  
-	}
-
-	/**
-	* Localization instance
-	* 
-	*/
-	public static function instance()
-	{
-		if (null === static::$instance) {
-			static::$instance = new self();
+	    if (file_exists($this->translationFile)) {
+		static::$translations = parse_ini_file($this->translationFile);
+	    } else {
+		if (file_exists($default = static::$settings['path'] . '/' . $this->getDefaultLanguage() . '.ini')) {
+		    copy($default, $this->translationFile);
 		}
+	    }  
 
-		return static::$instance;
+	} else {
+
+	    printf('Directory <strong>%s</strong> not exists, check path setting', dirname($this->translationFile)); 
+	    exit;
+
+        }  
+    }
+
+    /**
+    * Localization instance
+    * 
+    */
+    public static function instance()
+    {
+	if (null === static::$instance) {
+	    static::$instance = new self();
 	}
 
-
-	/**
-	* Get default language
-	* first element in the languages array
-	* 
-	* @return string 
-	*/
-	public function getDefaultLanguage()
-	{
-		$languages = static::$settings['languages'];
-
-		reset($languages);
-
-		return key($languages);
-	}
+	return static::$instance;
+    }
 
 
-	/**
-	* Get current language
-	* 
-	* @return string
-	*/
-	public function getCurrentLanguage()
-	{
-		if (isset($_GET[static::$settings['input']])) {
+    /**
+    * Get default language
+    * first element in the languages array
+    * 
+    * @return string 
+    */
+    public function getDefaultLanguage()
+    {
+        $languages = static::$settings['languages'];
 
-			if (array_key_exists($_GET[static::$settings['input']], $this->getLanguages())) {
-				return $_GET[static::$settings['input']];     
-			}
+	reset($languages);
 
-		}
+	return key($languages);
+    }
 
-		return $this->getDefaultLanguage(); 
-	}
 
-	/**
-	* Get all languages
-	* 
-	* @return array
-	*/
-	public function getLanguages()
-	{
-		return static::$settings['languages'];
-	}
+    /**
+    * Get current language
+    * 
+    * @return string
+    */
+    public function getCurrentLanguage()
+    {
+	if (isset($_GET[static::$settings['input']])) {
 
-	/**
-	* Get translations
-	* 
-	* @param array $strings
-	* @return {$string|$string[]}
-	*/
-	public function getTranslations($strings = [])
-	{
-		if (empty($strings)) {
-			return static::$translations;
-		}
-
-		$translations = [];
-
-		foreach ($strings as $string) {
-			$translations[$string] = isset(static::$translations[$string]) ? static::$translations[$string] : $this->translate($string);            
-		}
-
-		return $translations;
+	    if (array_key_exists($_GET[static::$settings['input']], $this->getLanguages())) {
+	        return $_GET[static::$settings['input']];     
+	    }
 
 	}
 
-	/**
-	* Get json
-	* 
-	* @return json
-	*/ 
-	public function getJson($strings = [], $options = JSON_UNESCAPED_UNICODE)
-	{
-		return json_encode($this->getTranslations($strings), $options);   
+	return $this->getDefaultLanguage(); 
+     }
+
+    /**
+    * Get all languages
+    * 
+    * @return array
+    */
+    public function getLanguages()
+    {
+	return static::$settings['languages'];
+    }
+
+    /**
+    * Get translations
+    * 
+    * @param array $strings
+    * @return {$string|$string[]}
+    */
+    public function getTranslations($strings = [])
+    {
+	if (empty($strings)) {
+	    return static::$translations;
 	}
 
-	/**
-	* Translate string
-	* 
-	* @param string $string
-	* @param array $args
-	*/
-	public function translate($string, $args = null)
-	{      
-		if (array_key_exists($string, static::$translations)) {
-			return is_array($args) ? vsprintf(static::$translations[$string], $args) : static::$translations[$string];
-		}
+	$translations = [];
 
-		file_put_contents(static::$translationFile, PHP_EOL . "{$string} = \"{$string}\"", FILE_APPEND | LOCK_EX);      
-
-		return is_array($args) ? vsprintf($string, $args) : static::$translations[$string] = $string;    
+	foreach ($strings as $string) {
+	    $translations[$string] = isset(static::$translations[$string]) ? static::$translations[$string] : $this->translate($string);            
 	}
+
+	return $translations;
+
+    }
+
+    /**
+    * Get json
+    * 
+    * @return json
+    */ 
+    public function getJson($strings = [], $options = JSON_UNESCAPED_UNICODE)
+    {
+	return json_encode($this->getTranslations($strings), $options);   
+    }
+
+    /**
+    * Translate string
+    * 
+    * @param string $string
+    * @param array $args
+    */
+    public function translate($string, $args = null)
+    {      
+	if (array_key_exists($string, static::$translations)) {
+	    return is_array($args) ? vsprintf(static::$translations[$string], $args) : static::$translations[$string];
+	}
+
+	file_put_contents(static::$translationFile, PHP_EOL . "{$string} = \"{$string}\"", FILE_APPEND | LOCK_EX);      
+
+	return is_array($args) ? vsprintf($string, $args) : static::$translations[$string] = $string;    
+    }
 
 }
 
@@ -158,5 +158,5 @@ class Localization {
 */
 function __($string, $args = null)
 {   
-	return Localization::instance()->translate($string, $args);
+    return Localization::instance()->translate($string, $args);
 }
