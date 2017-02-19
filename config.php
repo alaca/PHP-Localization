b59e7c9a3ed3a83605b462f77eefb77400d88a59
alaca <ante.laca@gmail.com>
2017-02-19 09:51:21 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,6 @@
 return [
    'path'  => 'translations',  // translations directory path
    'input' => 'language',      // url parameter
-   'language' => null,         // set current language 'en','de', etc. if null input param will be used to get current language 
    'languages' => [
       'en' => 'English'        // first element is default language 
    ]
